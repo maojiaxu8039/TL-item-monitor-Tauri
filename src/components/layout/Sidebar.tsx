1fb1af421963de778792512f0f458233f297db2e
mc <mc@MacBook-Air-2464.local>
2026-05-14 01:28:24 +0800

    左下角logo调整为44px

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -68,7 +68,7 @@ export function Sidebar({ page, onPageChange }: { page: PageId; onPageChange: (p
       </nav>
 
       <div className="torch-sidebar-footer">
-        <img src="/torchscan/logo-mark.png" alt="TorchScan" className="h-14 w-auto" draggable={false} />
+        <img src="/torchscan/logo-mark.png" alt="TorchScan" className="h-11 w-auto" draggable={false} />
       </div>
     </motion.aside>
   )
